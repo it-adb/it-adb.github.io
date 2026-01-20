67c62ccffcdbbb2a2c4008fafc1a02e0053d4039
arfan <marfanw@gmail.com>
2026-01-20 21:58:57 +0700

    Deploy 20/01/26, 21.58.25 - Attendance active by lecturer. Class recap feature.

---
+++ assets/useLecturerAssignments-BNW1KFc6.js
@@ -0,0 +1,2 @@
+import{u as r}from"./query-vendor-B--u1oZD.js";import{b as s}from"./index-C1kkCffi.js";function n(e){return r({queryKey:["lecturer-assignments",e],queryFn:()=>s.getLecturerAssignments(e)})}export{n as u};
+//# sourceMappingURL=useLecturerAssignments-BNW1KFc6.js.map
