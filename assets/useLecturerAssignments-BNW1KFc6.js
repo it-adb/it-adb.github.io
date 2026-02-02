86ee0cc69d2ec291b423e77800d79f3bcdbbf37e
Arfan <marfanw@gmail.com>
2026-02-02 17:43:17 +0700

    Deploy 02/02/26, 17.43.01 - update newer structure

--- assets/useLecturerAssignments-BNW1KFc6.js
+++
@@ -1,2 +0,0 @@
-import{u as r}from"./query-vendor-B--u1oZD.js";import{b as s}from"./index-C1kkCffi.js";function n(e){return r({queryKey:["lecturer-assignments",e],queryFn:()=>s.getLecturerAssignments(e)})}export{n as u};
-//# sourceMappingURL=useLecturerAssignments-BNW1KFc6.js.map
