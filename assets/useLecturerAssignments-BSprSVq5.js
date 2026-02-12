85f14f967f72e3087d0a05963290158996a9939d
Arfan <marfanw@gmail.com>
2026-02-12 17:07:53 +0700

    Deploy 12/02/26, 17.07.31 - Fix class summary

--- assets/useLecturerAssignments-BSprSVq5.js
+++
@@ -1,2 +0,0 @@
-import{u as r}from"./query-vendor-B--u1oZD.js";import{e as s}from"./index-BYG0ekbM.js";function n(e){return r({queryKey:["lecturer-assignments",e],queryFn:()=>s.getLecturerAssignments(e)})}export{n as u};
-//# sourceMappingURL=useLecturerAssignments-BSprSVq5.js.map
---
+++ assets/useLecturerAssignments-BhNgK-rR.js
@@ -0,0 +1,2 @@
+import{u as r}from"./query-vendor-B--u1oZD.js";import{e as s}from"./index-ysxJ7eO1.js";function n(e){return r({queryKey:["lecturer-assignments",e],queryFn:()=>s.getLecturerAssignments(e)})}export{n as u};
+//# sourceMappingURL=useLecturerAssignments-BhNgK-rR.js.map
