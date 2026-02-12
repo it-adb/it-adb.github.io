23c4ba077fe419b5ef701507a871717b141f7e43
Arfan <marfanw@gmail.com>
2026-02-12 15:52:50 +0700

    Deploy 12/02/26, 15.52.29 - Fix class summary

--- assets/useLecturerAssignments-BaTCYOa9.js
+++
@@ -1,2 +0,0 @@
-import{u as r}from"./query-vendor-B--u1oZD.js";import{d as s}from"./index-CU8_O-g9.js";function n(e){return r({queryKey:["lecturer-assignments",e],queryFn:()=>s.getLecturerAssignments(e)})}export{n as u};
-//# sourceMappingURL=useLecturerAssignments-BaTCYOa9.js.map
